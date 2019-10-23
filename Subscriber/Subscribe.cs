c1c7348c5ee5609fa69d4f1d91a9f519447ed989
Ganesha Nageshappa <Ganesha.Nageshappa@powerschool.com>
2019-10-23 15:33:42 +0530

    Initial Commit

---
+++ Subscriber/Subscribe.cs
@@ -0,0 +1,26 @@
+﻿using EasyNetQ;
+using Messages;
+using System;
+
+namespace Subscriber
+{
+    class Subscribe
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost:5672;username=admin;password=admin;prefetchcount=10;virtualHost=/;timeout=60"))
+            {
+                bus.Subscribe<TextMessage>("Test", HandleTextMessage);
+                Console.WriteLine("Listening for messages. Hit 'Return' to quit");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleTextMessage(TextMessage textMessage)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got Message : {0}", textMessage.Text);
+            Console.ResetColor();
+        }
+    }
+}
