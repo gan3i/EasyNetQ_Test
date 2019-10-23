6bababcbd669aa29a916be47287609f14a9d9355
Ganesha Nageshappa <Ganesha.Nageshappa@powerschool.com>
2019-10-23 21:03:28 +0530

    sendoutmessages added

--- Publisher/Publish.cs
+++ Publisher/Publish.cs
@@ -10,16 +10,35 @@ namespace Publisher
         {
             using (var bus = RabbitHutch.CreateBus("host=localhost:5672;username=admin;password=admin;prefetchcount=10;virtualHost=/;timeout=60"))
             {
-                var intput = "";
-                Console.WriteLine("Enter a message. 'Quit' to quit");
-                while ((intput = Console.ReadLine()) != "Quit")
-                {
-                    bus.Publish(new TextMessage
-                    {
-                        Text = intput
-                    });
-                }
+                SendOutMessages(bus);
+                ReadAndSend(bus);
             }
         }
+
+
+        private static void SendOutMessages(IBus bus)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                bus.Publish(new TextMessage
+                {
+                    Text = i + "-Hello!!"
+                });
+            }
+        }
+
+        static void ReadAndSend(IBus bus)
+        {
+            var intput = "";
+            Console.WriteLine("Enter a message. 'Quit' to quit");
+            while ((intput = Console.ReadLine()) != "Quit")
+            {
+                bus.Publish(new TextMessage
+                {
+                    Text = intput
+                });
+            }
+        }
+
     }
 }
