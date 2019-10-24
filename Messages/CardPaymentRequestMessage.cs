ea2b1c0bc6b82ef03eae711922c70d41b9e631da
Ganesha Nageshappa <Ganesha.Nageshappa@powerschool.com>
2019-10-24 22:53:07 +0530

    Card payment class

---
+++ Messages/CardPaymentRequestMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Messages
+{
+    public class CardPaymentRequestMessage
+    {
+        public string  CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public string ExpiryDate { get; set; }
+        public string Amount { get; set; }
+    }
+}
